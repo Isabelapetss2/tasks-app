fa06175890103cf86d9829e32a7ef45ce939e999
isabelatigs <isabelatigs@gmail.com>
2024-09-03 23:41:19 -0300

    ajuste de responsividade

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -8,7 +8,7 @@ function TaskPage() {
     const description = searchParams.get("description")
     return (
         <div className="w-screen h-screen bg-slate-500 p-6 flex justify-center">
-            <div className="w-[500px] space-y-4">
+            <div className="w-[500px] mx-auto space-y-4">
                 <div className="flex justify-center relative mb-6 text-slate-100">
                     <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0">
                         <ChevronLeftIcon />
